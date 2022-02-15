15d20f3fab5a3e5e81b55ccf64f4851e34f50a69
Joseph Heitzmann <josephheitzmann2@gmail.com>
2022-02-14 18:10:19 -0600

    practice from 1 on 1

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ let fullName = "Joseph Heitzmann";
 // Exercise 3
 let myStory =
   "Hello! My name is ${josephHeitzmann}. I live in ${southMississippi}. I play ${drums} in my free time.";
+
+
+  // const y = x++;
+  // //=
+  // const y = x;
+  // x = x + 1;
+
+
+  // const y = ++x;
+
+  // // = 
+  // x = x + 1;
+  // const y = x;
+
+  function hello () {
+    let x = 1;
+
+    console.log(x);
+  }
+
+  function hello2() {
+    let x = 2;
+
+    if (x === 2)  {
+      let y = 3;
+    }
+
+    console.log(y);
+  }
